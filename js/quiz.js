ae04189b7f86dc8751291e1f72998ac1c1495764
sheremyova <sheremjevadi@gmail.com>
2021-09-12 21:11:57 +0300

    Fixed the counter

--- js/quiz.js
+++ js/quiz.js
@@ -18,15 +18,19 @@ continue_btn.onclick = () => {
     info_box.classList.remove("activeInfo"); 
     quiz_box.classList.add("activeQuiz"); 
     showQuestions(0);
+    done_tasks_counter(1);
 }
 
 let que_count = 0;
+let que_numb = 1;
 const next_btn = document.querySelector(".next_button");
 
 next_btn.onclick = () => {
     if (que_count < questions.length - 1) {
         que_count++;
+        que_numb++;
         showQuestions(que_count);
+        done_tasks_counter(que_numb );
     } else {
         console.log("Questions completed");
     }
@@ -43,3 +47,10 @@ function showQuestions(index) {
     que_text.innerHTML = que_tag;
     option_list.innerHTML = option_tag; 
 }
+
+function done_tasks_counter(index){
+    const done_tasks = quiz_box.querySelector(".total");
+    let done_tasks_tag = '<span><p>' + index + '</p>of<p>' + questions.length + '</p></span>';
+    done_tasks.innerHTML = done_tasks_tag;
+}
+
